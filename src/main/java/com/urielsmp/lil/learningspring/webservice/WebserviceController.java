b7db40aa4d5456c449bf7cd4666b25f7d5f8fe30
urielsmp128 <urielsmp128@outlook.com>
2024-05-17 20:36:50 -0600

    Adding restful controller intro

---
+++ src/main/java/com/urielsmp/lil/learningspring/webservice/WebserviceController.java
@@ -0,0 +1,31 @@
+package com.urielsmp.lil.learningspring.webservice;
+
+import com.urielsmp.lil.learningspring.business.ReservationService;
+import com.urielsmp.lil.learningspring.business.RoomReservation;
+import com.urielsmp.lil.learningspring.util.DateUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class WebserviceController {
+
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public WebserviceController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservations(@RequestParam(value = "date", required = false) String dateString){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        return  this.reservationService.getRoomReservationsForDate(date);
+    }
+}
